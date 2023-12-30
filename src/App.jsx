4a4c6dff1a06a1624283f7e384908a599136bbe5
Cody <codymorales0406@icloud.com>
2023-12-29 15:44:59 -1000

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
 import { useState } from 'react'
 import {puppyList} from './data.js'
+import './App.css'
+
 
 // console.log(puppyList);
 
