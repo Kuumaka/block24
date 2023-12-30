d32de1f565534da17c1728b9407e4cecb64cb95b
Cody <codymorales0406@icloud.com>
2023-12-29 14:56:37 -1000

    added puppy array

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import {puppyList} from './data.js'
+
+console.log(puppyList);
 
 function App() {
   // const [count, setCount] = useState(0)
